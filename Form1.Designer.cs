9033832fb26e7c1ecdd11dd3fc1ee397a5088e72
Halim Whiteside <hwhitesi@students.solano.edu>
2025-04-09 14:05:51 -0700

    created new form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,9 +40,10 @@
             this.AppTitle.AutoSize = true;
             this.AppTitle.Font = new System.Drawing.Font("Myanmar Text", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AppTitle.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.AppTitle.Location = new System.Drawing.Point(216, 153);
+            this.AppTitle.Location = new System.Drawing.Point(288, 188);
+            this.AppTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.AppTitle.Name = "AppTitle";
-            this.AppTitle.Size = new System.Drawing.Size(160, 51);
+            this.AppTitle.Size = new System.Drawing.Size(201, 66);
             this.AppTitle.TabIndex = 0;
             this.AppTitle.Text = "VanGuard";
             // 
@@ -52,22 +53,24 @@
             this.AppDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.AppDescription.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AppDescription.ForeColor = System.Drawing.SystemColors.Control;
-            this.AppDescription.Location = new System.Drawing.Point(225, 207);
+            this.AppDescription.Location = new System.Drawing.Point(300, 255);
+            this.AppDescription.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AppDescription.Multiline = true;
             this.AppDescription.Name = "AppDescription";
             this.AppDescription.ReadOnly = true;
-            this.AppDescription.Size = new System.Drawing.Size(225, 70);
+            this.AppDescription.Size = new System.Drawing.Size(300, 86);
             this.AppDescription.TabIndex = 1;
-            this.AppDescription.Text = "A thoughtful combination of design and function to support your everyday movement" +
-    ". Limited stocks. Shop now.";
+            this.AppDescription.Text = "Where managing your delivery fleet is made easy with reporting tools, data manage" +
+    "ment, and live status update \r\nfeatures.";
             // 
             // CreateAccount
             // 
             this.CreateAccount.BackColor = System.Drawing.Color.OrangeRed;
             this.CreateAccount.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CreateAccount.Location = new System.Drawing.Point(225, 301);
+            this.CreateAccount.Location = new System.Drawing.Point(300, 370);
+            this.CreateAccount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CreateAccount.Name = "CreateAccount";
-            this.CreateAccount.Size = new System.Drawing.Size(132, 31);
+            this.CreateAccount.Size = new System.Drawing.Size(176, 38);
             this.CreateAccount.TabIndex = 2;
             this.CreateAccount.Text = "create account";
             this.CreateAccount.UseVisualStyleBackColor = false;
@@ -75,9 +78,10 @@
             // Login
             // 
             this.Login.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Login.Location = new System.Drawing.Point(374, 301);
+            this.Login.Location = new System.Drawing.Point(499, 370);
+            this.Login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(132, 31);
+            this.Login.Size = new System.Drawing.Size(176, 38);
             this.Login.TabIndex = 3;
             this.Login.Text = "log in";
             this.Login.UseVisualStyleBackColor = true;
@@ -87,22 +91,24 @@
             this.LPpanel.BackColor = System.Drawing.Color.OrangeRed;
             this.LPpanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.LPpanel.Location = new System.Drawing.Point(0, 0);
+            this.LPpanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LPpanel.Name = "LPpanel";
-            this.LPpanel.Size = new System.Drawing.Size(800, 49);
+            this.LPpanel.Size = new System.Drawing.Size(1067, 60);
             this.LPpanel.TabIndex = 4;
             this.LPpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.LPpanel_Paint);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.LPpanel);
             this.Controls.Add(this.Login);
             this.Controls.Add(this.CreateAccount);
             this.Controls.Add(this.AppDescription);
             this.Controls.Add(this.AppTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
