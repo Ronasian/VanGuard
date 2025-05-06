7a53b38e9f7c084635717039a83834a653264735
Halim Whiteside <hwhitesi@students.solano.edu>
2025-05-06 03:07:12 -0700

    Fixed the create account button

--- StartingPage.cs
+++ StartingPage.cs
@@ -8,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GroupProject.Models;
+using GroupProject.Data;
+
 //testing
 namespace GroupProject
 {
@@ -16,6 +19,15 @@ namespace GroupProject
         public StartingPage()
         {
             InitializeComponent();
+
+            //Sets up dropdown role options
+            cmbRole.Items.Add("Driver");
+            cmbRole.Items.Add("Manager");
+
+            //Default view
+            panelLogin.Visible = true;
+            panelCreateAccount.Visible = false;
+          
             LPpanel.Invalidate();
             this.DoubleBuffered = true;
 
@@ -57,9 +69,10 @@ namespace GroupProject
 
         private void btnCreateAccount_Click(object sender, EventArgs e)
         {
+            panelLogin.Visible = false;
+            panelCreateAccount.Visible = true;
 
         }
-
         private void panelCreateAccount_Paint(object sender, PaintEventArgs e)
         {
 
@@ -78,6 +91,38 @@ namespace GroupProject
         private void label7_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnSubmitCreate_Click(object sender, EventArgs e)
+        {
+            //reads inputs from the form
+            string name = txtName.Text.Trim();
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+            string role = cmbRole.SelectedItem?.ToString(); // role selected from dropdown list
+
+            MessageBox.Show($"Name: '{name}'\nEmail: '{email}'\nPassword: '{password}'\nRole: '{role}");
+
+            //checks fields are not empty
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(email) ||
+                string.IsNullOrEmpty(password) || string.IsNullOrEmpty(role))
+            {
+                MessageBox.Show("All fields are required.");
+                return;
+            }
+
+            //check if email hjas been used
+            if (UserDatabase.Users.Any(u => u.Email == email))
+            {
+                MessageBox.Show("Email is already in use.");
+                return;
+            }
+
+            //Creates a new user and adds to the database
+            var newUser = new User(name, email, password, role);
+            UserDatabase.AddUser(newUser);
+
+            MessageBox.Show("Account created successfully!");
         }
     }
 }
